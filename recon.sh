17673ddfb99dfc70d92dada432e4a7fd746f7604
Ksmith <51425481+GOH4N@users.noreply.github.com>
2021-08-16 23:46:57 +0100

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+# Created by HeckSec
+
 url=$1
 
 if [ ! -d "$url" ];then
-	mkdir $url
+        mkdir $url
 fi
 
 if [ ! -d "$url/recon" ];then
-	mkdir $url/recon
+        mkdir $url/recon
 fi
 
-echo "[+] Harvesting Subdomains with Assetfinder..."
-assetfinder --subs-only $url >> $url/recon/assets.txt
-cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
-rm $url/recon/assets.txt 
+echo "[+] Gathering Subdomains with Sublist3r..."
+sublist3r -d $url -o $url/recon/subdomains.txt
+sort -u $url/recon/subdomains.txt >> $url/recon/initial.txt # this removes duplicates and saves to a new file 
+rm $url/recon/subdomains.txt # removes old file with possible duplicates
 
-# echo "[+] Harvesting Subdomains with Owasp Amass..."
-# amass enum -d $url >> $url/recon/f.txt 
-# sort -u $url/recon/assets.txt >> $url/recon/final.txt
-# rm $url/recon/f.txt
 
-echo "[+] Probing for Alive Domains..."
-cat $url/recon/final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/alive.txt
+sed -i -e 's_^_https://_' $url/recon/initial.txt # this adds https: to the beginning of each line as the following tools appreciate the formality. 
+
+echo "[+] Probing for alive domains with probethis..."
+cat $url/recon/initial.txt | probethis.py -s 200,401 -o $url/recon/alive_subs.txt
+
+echo "[+] Probing for Directories with feroxbuster.."
+cat $url/recon/alive_subs.txt | feroxbuster --stdin --silent -s 200 401 --depth 5 -x php,html,js,json,docx,xlxs,csv,txt,aspx  -q --wordlist /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-small.txt -o $url/recon/dirs.txt
+# CHANGE WORDLIST, FILEPATH, & DEPTH IF NECESSARY
+
+sort -u $url/recon/dirs.txt >> $url/recon/fin_dirs.txt
+rm $url/recon/dirs.txt 
